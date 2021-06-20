2fdf2c3ec59c805e4a4f5da77a8364b3bfc4ecc8
GuangJianNing <34411304+GuangJianNing@users.noreply.github.com>
2021-06-20 20:12:54 +0800

    Update app.java
    在github的master 分支上写了一些东西

--- app.java
+++ app.java
@@ -1,5 +1,6 @@
 public void main(){
   //writen by  a
   System.out.println("hello");//修正一个错误，在github端	
-  System.out.println("hello2")//writen by a
+  System.out.println("hello2");//writen by a
+  System.out.println("hello in master");// writen in github master
 }
